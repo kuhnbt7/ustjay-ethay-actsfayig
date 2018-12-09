ed24869cbc30e879956281b2645fc2625df2cebe
kuhnbt <kuhnbt@gmail.com>
2018-12-09 12:30:34 -0500

    changes

--- main.py
+++ main.py
@@ -14,13 +14,20 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
 
+def get_pig_latin_url(txt):
+
+	pig_latin_url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+	data = {'input_text':txt}
+	result = requests.post(pig_latin_url, data=data)
+	return '<a href=' + str(result.url) + '>Click here!</a>'
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    new_fact = get_fact()
+    pig_latin_url = get_pig_latin_url(new_fact)
+    return str('<a>'+pig_latin_url+'</a>')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
